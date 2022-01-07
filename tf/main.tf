5bffff3cb17c50ff09f805c11c1856d8171dfb80
Steven O <steveno@liatrio.com>
2022-01-07 10:56:04 -0600

    added github provider

--- tf/main.tf
+++ tf/main.tf
@@ -2,11 +2,19 @@ provider "azurerm" {
   features {}
 }
 
+provider "github" {
+  owner = "liatrio-tech-challenge"
+}
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>2.91"
     }
+  }
+  github = {
+    source  = "integrations/github"
+    version = "~>4.19.1"
   }
 }
