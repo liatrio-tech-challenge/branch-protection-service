61ab5fa97ad1f87af46d4a591367870061662f17
Steven O <steveno@liatrio.com>
2022-01-07 11:23:12 -0600

    adding path to future url

--- tf/github.tf
+++ tf/github.tf
@@ -1,6 +1,6 @@
 resource "github_organization_webhook" "branch_protection_service" {
   configuration {
-#    url          = "https://${azurerm_function_app.branch_protection_service.name}.azurewebsites.net"
+#    url          = "https://${azurerm_function_app.branch_protection_service.name}.azurewebsites.net/api/branch-protection"
     url          = "https://ghe-org-mgmt.ngrok.io/api/branch-protection"
     content_type = "json"
     insecure_ssl = false
