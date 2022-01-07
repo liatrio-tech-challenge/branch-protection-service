97615486c3995ddbb8732886102b8be3c9891125
Steven O <steveno@liatrio.com>
2022-01-07 14:25:32 -0600

    updated app settings for linux

--- tf/function_app.tf
+++ tf/function_app.tf
@@ -15,10 +15,12 @@ resource "azurerm_app_service_plan" "branch_protection_service" {
   name                = var.function_app_name
   location            = azurerm_resource_group.github_tech_challenge.location
   resource_group_name = azurerm_resource_group.github_tech_challenge.name
+  kind                = "Linux"
+  reserved            = true
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Dynamic"
+    size = "Y1"
   }
 }
 
@@ -30,11 +32,16 @@ resource "azurerm_function_app" "branch_protection_service" {
   storage_account_name       = azurerm_storage_account.branch_protection_service.name
   storage_account_access_key = azurerm_storage_account.branch_protection_service.primary_access_key
 
+  site_config {
+    linux_fx_version = "node|14"
+  }
+  os_type = "linux"
+  version = "~3"
+
   app_settings = {
     WEBHOOK_SECRET : var.github_webhook_secret
     GITHUB_TOKEN : var.github_enforcer_pat
     FUNCTIONS_WORKER_RUNTIME : "node"
     WEBSITE_RUN_FROM_PACKAGE : ""
-    WEBSITE_NODE_DEFAULT_VERSION : "node|14"
   }
 }
